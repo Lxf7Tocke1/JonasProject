9bf56a5b923e9ab67959760af658f01e6aa39815
Lxf7Tocke1 <lxf7tocke@gmail.com>
2025-09-27 13:43:56 +0200

    addenemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float Speed;
+    [SerializeField] float Health;
+    [SerializeField] float MaxHealth = 10f;
+
+
+    Transform PlayerPosition;
+    Vector3 EnemyToPlayer;
+    void Start()
+    {
+        Health = MaxHealth;
+        Speed = 1f;
+        PlayerPosition = FindAnyObjectByType<PlayerMovement>().transform;
+    }
+
+    void Update()
+    {
+        EnemyToPlayer = (PlayerPosition.position - transform.position).normalized * Time.deltaTime * Speed;
+        transform.position += EnemyToPlayer;
+
+    }
+}
