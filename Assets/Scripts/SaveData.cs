cf7fcbebf867aeb42f8665f609582f20922777fe
Lxf7Tocke1 <lxf7tocke@gmail.com>
2025-10-10 21:49:53 +0200

    no sound added

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -1,45 +1,45 @@
-using unityengine;
+//using unityengine;
 
-public class savedata : Monobehaviour
-{
-    public struct savedata
-    {
-        public int highscore;
-    }
+//public class savedata : Monobehaviour
+//{
+//    public struct savedata
+//    {
+//        public int highscore;
+//    }
 
-    void start()
-    {
+//    void start()
+//    {
 
-    }
-    public class savemanager : monobehaviour
-    {
-        [serializefield] private savedata data;
-        [serializefield] string filename;
+//    }
+//    public class savemanager : monobehaviour
+//    {
+//        [serializefield] private savedata data;
+//        [serializefield] string filename;
 
-        public int gethighscore => data.highscore;
-        string getpath()
-        {
-            return application.persistentdatapath + "/" + filename + ".json"
-        }
-        private void loaddata()
-        {
-            if (!filename.exists(getpath()))
-            {
-                savegamefile();
-                return;
-            }
-        }
-        private void savegamefile()
-        {
-            string jsonfile = jsonutility.tojson(data, true);
-            filename.writealltext(getpath(), jsonfile);
-        }
-        public void sethighscore(int somescore)
-        {
-            if (data.highscore > somescore) return;
+//        public int gethighscore => data.highscore;
+//        string getpath()
+//        {
+//            return application.persistentdatapath + "/" + filename + ".json"
+//        }
+//        private void loaddata()
+//        {
+//            if (!filename.exists(getpath()))
+//            {
+//                savegamefile();
+//                return;
+//            }
+//        }
+//        private void savegamefile()
+//        {
+//            string jsonfile = jsonutility.tojson(data, true);
+//            filename.writealltext(getpath(), jsonfile);
+//        }
+//        public void sethighscore(int somescore)
+//        {
+//            if (data.highscore > somescore) return;
 
-            data.highscore = somescore;
-            savegamefile();
-        }
-    }
-}
+//            data.highscore = somescore;
+//            savegamefile();
+//        }
+//    }
+//}
